bc0a58ed0bf0de2f3f8b6c903d64ee70161a8c1e
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-01-29 14:13:19 +0100

    Modification de music pour prendre en compte les cookie pour youtube

--- cogs/music_cog.py
+++ cogs/music_cog.py
@@ -17,6 +17,7 @@ ytdl_format_options = {
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
+    'cookiesfrombrowser': ('chrome',),
     'default_search': 'auto',
     'source_address': '0.0.0.0',
     'postprocessors': [{
